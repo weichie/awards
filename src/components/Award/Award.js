341ca0ee97f8ea974ae55e92192ec0b134b61209
Bob Weichler <bobmasaki@hotmail.com>
2018-07-23 07:23:46 +0200

    Add Award website link + nofollow choice

--- src/components/Award/Award.js
+++ src/components/Award/Award.js
@@ -5,26 +5,29 @@ class Award extends React.Component{
 	render(){
 		const a = this.props.award;
 		let type = (a.award_type === 'client: Client Work') ? 'Subcontract' : 'Self Made';
+		let rel = (a.allow_follow === '1') ? 'noopener noreferrer' : 'noopener noreferrer noindex nofollow';
 
 		return(
 			<li className="award" style={{background: `url(${a.featured_img_src})`}}>
-				<div className="award-filter"></div>
-				<div className="top">
-					<strong>{type}</strong>
-					<small>{a.award_duration}</small>
-				</div>
-				<span className="score">{a.award_score}</span>
-				<div className="bottom">
-					<h4>{a.title}</h4>
-					<div className="author">
-						<div className="author-img" style={{background: `url(${a.award_company_image})`}}>
-						</div>
-						<div className="text">
-							<strong>{a.award_company_name}</strong>
-							<small>{a.award_programming_language}</small>
+				<a className="project-link" href={a.award_url} target="_blank" rel={rel}>
+					<div className="award-filter"></div>
+					<div className="top">
+						<strong>{type}</strong>
+						<small>{a.award_duration}</small>
+					</div>
+					<span className="score">{a.award_score}</span>
+					<div className="bottom">
+						<h4>{a.title}</h4>
+						<div className="author">
+							<div className="author-img" style={{background: `url(${a.award_company_image})`}}>
+							</div>
+							<div className="text">
+								<strong>{a.award_company_name}</strong>
+								<small>{a.award_programming_language}</small>
+							</div>
 						</div>
 					</div>
-				</div>
+				</a>
 			</li>
 		);
 	}
